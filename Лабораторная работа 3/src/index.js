c618e6db16cf555eea0593d600f2a1571752f9a2
Alexander Volzhanin <alexandervolzhanin2004@gmail.com>
2024-06-02 18:57:05 +0300

    update

---
+++ Лабораторная работа 3/src/index.js
@@ -0,0 +1,4 @@
+import 'bootstrap';
+import './styles.scss';
+
+console.log('Portfolio site is up and running!');
---
+++ Лабораторная работа 3/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,  // Эта опция будет очищать папку dist перед каждым билдом
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.css'
+    })
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+    open: true,  // Эта опция автоматически откроет браузер при запуске сервера
+  }
+};
